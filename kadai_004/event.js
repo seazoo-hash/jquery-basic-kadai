ea970e520de69bc0992722d88ba6c24c389f9f99
seazoo-hash <155947973+seazoo-hash@users.noreply.github.com>
2024-01-31 23:30:20 +0900

    課題１

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+$(window).on("load", function () {
+    console.log("loadイベントが発生しました");
+});
+
+$(function () {
+    $(window).on({
+        'scroll': () => {
+            console.log('scrollイベントが発生しました');
+        },
+    });
+});
+
